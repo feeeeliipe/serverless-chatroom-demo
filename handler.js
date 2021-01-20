131b8786bc5a173975243ffabc71c59734916df6
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2021-01-19 21:51:50 -0300

    [FEAT]: Criando as rotas necessárias para a aplicação

--- handler.js
+++ handler.js
@@ -1,18 +1,61 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
-  return {
+module.exports.createChatMessage = async (event, context) => {
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      message: "createChatMessage",
+      method: event.httpMethod,
+      path: event.path,
+      query: event.queryStringParameters,
+      params: event.pathParameters,
+      body: event.body,
+    }),
   };
+  return response;
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.getMessage = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "getMessage",
+      method: event.httpMethod,
+      path: event.path,
+      query: event.queryStringParameters,
+      params: event.pathParameters,
+      body: event.body,
+    }),
+  };
+  return response;
+};
+
+module.exports.getRoomMessages = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "getRoomMessages",
+      method: event.httpMethod,
+      path: event.path,
+      query: event.queryStringParameters,
+      params: event.pathParameters,
+      body: event.body,
+    }),
+  };
+  return response;
+};
+
+module.exports.getUserMessages = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "getUserMessages",
+      method: event.httpMethod,
+      path: event.path,
+      query: event.queryStringParameters,
+      params: event.pathParameters,
+      body: event.body,
+    }),
+  };
+  return response;
 };
